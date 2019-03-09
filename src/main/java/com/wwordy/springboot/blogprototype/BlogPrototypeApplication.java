f53e2873059678df3a8978cf50d20dbbf96e6364
wwordy <2114919460@qq.com>
2019-03-09 17:19:08 +0800

    Initial commit

---
+++ src/main/java/com/wwordy/springboot/blogprototype/BlogPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.wwordy.springboot.blogprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPrototypeApplication.class, args);
+	}
+
+}
